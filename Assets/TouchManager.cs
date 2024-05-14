f2e9f5fc423f08c6f2e3a244e8d07dc0f1ed7032
Michael Hovgaard <hovgaard1990@gmail.com>
2024-05-14 12:45:26 +0200

    removed.

--- Assets/TouchManager.cs
+++ Assets/TouchManager.cs
@@ -1,25 +1,36 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class TouchManager : MonoBehaviour
 {
-
-    private PlayerInput playerInput;
-    private void Awake()
-    {
-        playerInput = GetComponent<PlayerInput>();
-        playerInput.Enable();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; Input.touchCount > i; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            if(touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch Began");
+
+                touch
+            }
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Debug.Log("Touch Moved");
+            }
+            if(touch.phase == TouchPhase.Stationary)
+            {
+                Debug.Log("Touch Stationary");
+            }
+            if(touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("Touch Ended");
+            }
+            if(touch.phase == TouchPhase.Canceled)
+            {
+                Debug.Log("Touch Canceled");
+            }
+        }
     }
 }
